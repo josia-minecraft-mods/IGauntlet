6c1e2107db12d507a97318d351808b134fe629f1
Josia50 <elwyn.vdb@gmail.com>
2019-01-14 18:22:34 +0100

    Made dust fall

--- src/main/java/com/common/blocks/AshPile.java
+++ src/main/java/com/common/blocks/AshPile.java
@@ -1,27 +1,39 @@
 package com.common.blocks;
 
 
+import com.Main;
+import com.init.ModBlocks;
+import com.init.ModItems;
 import com.tabs.InfinityTabs;
+import com.util.IHasModel;
+import net.minecraft.block.BlockFalling;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 import javax.annotation.Nullable;
 
-public class AshPile extends BlockBase {
+public class AshPile extends BlockFalling implements IHasModel {
 
     public static final AxisAlignedBB DUST_AABB = new AxisAlignedBB(0.296875,0,0.296875,0.6900,0.1875 / 2,0.6900);
 
     public AshPile(String name, Material material)
     {
-        super(name, material);
+        super(material);
+        setUnlocalizedName(name);
+        setRegistryName(name);
         setSoundType(SoundType.SAND);
         setHardness(0.0F);
         setResistance(0.1F);
         setCreativeTab(InfinityTabs.infinityTabs);
+
+        ModBlocks.BLOCKS.add(this);
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 
     @Override
@@ -48,5 +60,11 @@ public class AshPile extends BlockBase {
     @Override
     public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
         return DUST_AABB;
+    }
+
+    @Override
+    public void registerModels()
+    {
+        Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
     }
 }
