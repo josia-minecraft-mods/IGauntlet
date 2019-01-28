dac0ff8fb3ead781a0c3700f99f27db457351f89
Josia50 <elwyn.vdb@gmail.com>
2019-01-28 20:54:38 +0100

    Networking for Space Stone

--- src/main/java/com/network/NetworkHandler.java
+++ src/main/java/com/network/NetworkHandler.java
@@ -1,6 +1,8 @@
 package com.network;
 
+import com.network.packets.MessageNoCoords;
 import com.network.packets.MessageNotAdded;
+import com.network.packets.MessageSpace;
 import com.network.packets.MessageStone;
 import com.util.Reference;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
@@ -14,5 +16,7 @@ public class NetworkHandler {
     public static void init() {
         NETWORK.registerMessage(MessageStone.Handler.class, MessageStone.class, 1, Side.SERVER);
         NETWORK.registerMessage(MessageNotAdded.Handler.class, MessageNotAdded.class, 2, Side.SERVER);
+        NETWORK.registerMessage(MessageSpace.Handler.class, MessageSpace.class, 3, Side.SERVER);
+        NETWORK.registerMessage(MessageNoCoords.Handler.class, MessageNoCoords.class, 4, Side.SERVER);
     }
 }
