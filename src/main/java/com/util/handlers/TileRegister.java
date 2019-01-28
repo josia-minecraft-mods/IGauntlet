3effa084944ea13a9bc24219044a20af6cac0e40
Josia50 <elwyn.vdb@gmail.com>
2019-01-28 20:52:41 +0100

    Dangerous prefix resolved

--- src/main/java/com/util/handlers/TileRegister.java
+++ src/main/java/com/util/handlers/TileRegister.java
@@ -1,11 +1,12 @@
 package com.util.handlers;
 
 import com.common.tileentity.TileAshPile;
+import com.util.Reference;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class TileRegister {
 
     public static final void PreInit() {
-        GameRegistry.registerTileEntity(TileAshPile.class, "tile_ash_pile");
+        GameRegistry.registerTileEntity(TileAshPile.class, Reference.MODID + ":tile_ash_pile");
     }
 }
