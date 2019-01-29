1f67996e519eb207c8bf16499c0d6517a3cfa83c
Josia50 <elwyn.vdb@gmail.com>
2019-01-29 08:30:20 +0100

    Update to 2.5

--- src/main/java/com/util/Reference.java
+++ src/main/java/com/util/Reference.java
@@ -2,12 +2,12 @@ package com.util;
 
 public class Reference {
     public static final String NAME = "Infinity Mod";
-    public static final String VERSION = "2.4";
+    public static final String VERSION = "2.5";
     public static final String MODID = "infinity";
 
     public static final String CLIENTPROXY = "com.proxy.ClientProxy";
     public static final String COMMONPROXY = "com.proxy.CommonProxy";
-    public static final String UPDATEURL = "https://github.com/rootheworld/NewInfinityGauntlet/blob/master/update.json";
+    public static final String UPDATEURL = "https://raw.githubusercontent.com/rootheworld/NewInfinityGauntlet/master/update.json";
 
     public static final String BACKGROUND_TAB = "infinity.png";
 }
