cb836ad4bec878916c3ae65646e8496d0bdb9f6a
Josia50 <elwyn.vdb@gmail.com>
2019-02-02 20:38:20 +0100

    They were not needed - May gave error on join in existing world

--- src/main/java/com/igauntlet/init/ModItems.java
+++ src/main/java/com/igauntlet/init/ModItems.java
@@ -25,12 +25,6 @@ public class ModItems {
     public static final Item POWERSTONE = new PowerStone("power_stone");
     public static final Item SOULSTONE = new SoulStone("soul_stone");
 
-    public static final Item GREENSTONE = new ItemBase("stone_green", false);
-    public static final Item REDSTONE = new ItemBase("stone_red", false);
-    public static final Item BLUESTONE = new ItemBase("stone_blue", false);
-    public static final Item ORANGESTONE = new ItemBase("stone_orange", false);
-    public static final Item YELLOWSTONE = new ItemBase("stone_yellow", false);
-
     //Tools
     public static final Item DWARFHAMMER = new Dwarfhammer("dwarf_hammer");
 
