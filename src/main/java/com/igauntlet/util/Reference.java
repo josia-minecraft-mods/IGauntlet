169d29c7e1b61a968300059adf226dcc63e22211
Josia50 <elwyn.vdb@gmail.com>
2019-02-02 13:14:12 +0100

    Tesseract + Update 2.8

--- src/main/java/com/igauntlet/util/Reference.java
+++ src/main/java/com/igauntlet/util/Reference.java
@@ -2,7 +2,7 @@ package com.igauntlet.util;
 
 public class Reference {
     public static final String NAME = "Infinity Mod";
-    public static final String VERSION = "@VERSION";
+    public static final String VERSION = "2.8";
     public static final String MODID = "igauntlet";
 
     public static final String CLIENTPROXY = "com.igauntlet.proxy.ClientProxy";
