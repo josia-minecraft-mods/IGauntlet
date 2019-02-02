f3d39954abe7c320bbfce5114a1a841921e22666
Josia50 <elwyn.vdb@gmail.com>
2019-02-02 17:44:13 +0100

    Better Ore Generator

--- src/main/java/com/igauntlet/world/ModWorldGen.java
+++ src/main/java/com/igauntlet/world/ModWorldGen.java
@@ -1,42 +1,54 @@
 package com.igauntlet.world;
 
 import com.igauntlet.init.ModBlocks;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.state.pattern.BlockMatcher;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
 import java.util.Random;
 
-public class ModWorldGen implements IWorldGenerator
-{
+
+public class ModWorldGen implements IWorldGenerator {
+    private WorldGenerator uru_ore;
+
+
+    public ModWorldGen() {
+        uru_ore = new WorldGenMinable(ModBlocks.URU_ORE.getDefaultState(), 9, BlockMatcher.forBlock(Blocks.STONE));
+    }
+
     @Override
-    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkprovider)
-    {
-        if (world.provider.getDimension() == 0)
-        {
-            generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkprovider);
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        switch (world.provider.getDimension()) {
+            case -1:
+                break;
+
+            case 0:
+
+                runGenerator(uru_ore, world, random, chunkX, chunkZ, 20, 0, 30);
+                break;
+
+            case 1:
+                break;
         }
     }
 
-    private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkprovider)
-    {
-        generateOre(ModBlocks.URU_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16,64, random.nextInt(7) + 4, 3);
-    }
+    private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, int chance, int minHeight, int maxHeight) {
+        if (minHeight > maxHeight || minHeight < 0 || maxHeight > 256)
+            throw new IllegalArgumentException("Ore generated out of bounds");
 
-    private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances)
-    {
-        int deltaY = maxY- minY;
+        int heightDiff = maxHeight - minHeight + 1;
+        for (int i = 0; i < chance; i++) {
+            int x = chunkX * 16 + rand.nextInt(16);
+            int y = minHeight + rand.nextInt(heightDiff);
+            int z = chunkZ * 16 + rand.nextInt(16);
 
-        for (int i = 0; i < chances; i++)
-        {
-            BlockPos pos = new BlockPos(x + random.nextInt(16),minY + random.nextInt(deltaY), z + random.nextInt(16));
-
-            WorldGenMinable generator = new WorldGenMinable(ore, size);
-            generator.generate(world, random, pos);
+            gen.generate(world, rand, new BlockPos(x, y, z));
         }
     }
 }
