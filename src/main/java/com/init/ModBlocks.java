dcbedb526cdfd51a2177868b97ef8f29e305e065
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-02-02 13:43:50 -0600

    Dwarf Update

--- src/main/java/com/init/ModBlocks.java
+++ src/main/java/com/init/ModBlocks.java
@@ -1,7 +1,11 @@
 package com.init;
 
 import com.common.blocks.AshPile;
-
+import com.common.blocks.DwarfCobble;
+import com.common.blocks.DwarfDirt;
+import com.common.blocks.DwarfStone;
+import com.common.blocks.DwarfWood;
+import com.common.blocks.Forge;
 import com.common.blocks.Tesseract;
 import com.common.blocks.UruOre;
 import net.minecraft.block.Block;
@@ -17,5 +21,11 @@ public class ModBlocks {
     public static final Block ASH_PILE = new AshPile("ash_pile", Material.SAND, true);
     public static final Block URU_ORE = new UruOre("uru_ore", Material.ROCK, true);
     public static final Block TESSERACT = new Tesseract("tesseract", Material.CLAY, true);
+    public static final Block DWARF_STONE = new DwarfStone("dwarf_stone", Material.GROUND, true);
+    public static final Block DWARF_DIRT = new DwarfDirt("dwarf_dirt", Material.GRASS, true);
+    public static final Block DWARF_COBBLE = new DwarfCobble("dwarf_cobble", Material.GROUND, true);
+    public static final Block DWARF_WOOD = new DwarfWood("dwarf_wood", Material.WOOD, true);
+    public static final Block FORGE = new Forge("forge", Material.ANVIL, true);
+    
 
 }
