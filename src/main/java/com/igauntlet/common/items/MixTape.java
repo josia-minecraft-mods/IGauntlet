ac4b7e5bf7e68c76bbd0289fbae9edab88297ffc
Josia50 <elwyn.vdb@gmail.com>
2019-02-05 19:50:58 +0100

    better

--- src/main/java/com/igauntlet/common/items/MixTape.java
+++ src/main/java/com/igauntlet/common/items/MixTape.java
@@ -3,12 +3,13 @@ package com.igauntlet.common.items;
 import com.igauntlet.Infinity;
 import com.igauntlet.init.ModItems;
 import com.igauntlet.tabs.InfinityTabs;
-import com.igauntlet.util.handlers.SoundsHandler;
 import com.igauntlet.util.handlers.helpers.IHasModel;
 import net.minecraft.item.ItemRecord;
 import net.minecraft.util.SoundEvent;
 
 public class MixTape extends ItemRecord implements IHasModel {
+
+    SoundEvent getsound;
 
     public MixTape(String name, SoundEvent sound) {
         super(name, sound);
@@ -18,13 +19,16 @@ public class MixTape extends ItemRecord implements IHasModel {
         setMaxDamage(4500);
         setCreativeTab(InfinityTabs.infinityTabs);
 
+        getsound = sound;
         ModItems.ITEMS.add(this);
     }
 
     @Override
     public SoundEvent getSound() {
-        return SoundsHandler.SNAP;
+        return getsound;
     }
+
+
 
     @Override
     public void registerModels() {
