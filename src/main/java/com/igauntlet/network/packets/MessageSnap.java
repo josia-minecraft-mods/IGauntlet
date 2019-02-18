cf5867f51ebc4ca5590bb633300ae768350c8232
Josia50 <elwyn.vdb@gmail.com>
2019-02-18 20:06:48 +0100

    Fixed Snap....

--- src/main/java/com/igauntlet/network/packets/MessageSnap.java
+++ src/main/java/com/igauntlet/network/packets/MessageSnap.java
@@ -18,12 +18,16 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.igauntlet.common.items.function.gems.GemPower.WriteAsh;
 
 
 public class MessageSnap implements IMessage {
 
     public static boolean snap;
+    public static final List<EntityLiving> SNAPENTITY = new ArrayList<EntityLiving>();
 
     public MessageSnap() {
     }
@@ -44,12 +48,12 @@ public class MessageSnap implements IMessage {
 
         @Override
         public IMessage onMessage(MessageSnap message, MessageContext ctx) {
-
             ctx.getServerHandler().player.getServerWorld().addScheduledTask(() -> {
                 EntityPlayerMP playerIn = ctx.getServerHandler().player;
                 ItemStack stack = playerIn.getActiveItemStack();
                 boolean CanSnap = ModConfig.Gauntlet.Snap;
                 int extend = ModConfig.Gauntlet.ExtensionRange;
+                int passentity = 0;
                 NBTTagCompound nbt = stack.getTagCompound();
 
                 if (CanSnap) {
@@ -57,18 +61,21 @@ public class MessageSnap implements IMessage {
                     if (!(playerIn.getHeldItemMainhand().getItem() == ModItems.INFINITY_GAUNTLET)) return;
                     for (EntityLiving targetentity : playerIn.world.getEntitiesWithinAABB(EntityLiving.class, playerIn.getEntityBoundingBox().grow(extend, extend, extend))) {
                         int entity = targetentity.getEntityId();
+                        SNAPENTITY.add(targetentity);
+                        passentity++;
 
-                        if (!targetentity.getIsInvulnerable()) {
-                            Block blk = ModBlocks.ASH_PILE;
-                            BlockPos pos0 = new BlockPos(targetentity.posX, targetentity.posY, targetentity.posZ);
-                            IBlockState state0 = blk.getDefaultState();
-                            targetentity.world.setBlockState(pos0, state0);
-                            WriteAsh(pos0, playerIn.world, entity);
-                            targetentity.attackEntityFrom(IDamageSource.SNAP, targetentity.getMaxHealth() - targetentity.getHealth());
-                        }
+                            if (!targetentity.getIsInvulnerable()) {
+                                Block blk = ModBlocks.ASH_PILE;
+                                BlockPos pos0 = new BlockPos(targetentity.posX, targetentity.posY, targetentity.posZ);
+                                IBlockState state0 = blk.getDefaultState();
+                                targetentity.world.setBlockState(pos0, state0);
+                                WriteAsh(pos0, playerIn.world, entity);
+                                targetentity.attackEntityFrom(IDamageSource.SNAP, 1000);
+                            }
                     }
-                    playerIn.world.playSound(null, playerIn.getPosition(), SoundsHandler.SNAP, SoundCategory.HOSTILE, 1F, 1F);
-                }
+                        playerIn.world.playSound(null, playerIn.getPosition(), SoundsHandler.SNAP, SoundCategory.HOSTILE, 1F, 1F);
+                        passentity = 0;
+                    }
             });
             return null;
         }
