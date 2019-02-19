83a7117037ba4ee438a3a7d55d2f5915aa3dc3a3
Josia50 <elwyn.vdb@gmail.com>
2019-02-19 08:09:11 +0100

    / Not Power Stone , Is in Packet MessageSnap

--- src/main/java/com/igauntlet/common/items/function/gems/GemPower.java
+++ src/main/java/com/igauntlet/common/items/function/gems/GemPower.java
@@ -3,54 +3,17 @@ package com.igauntlet.common.items.function.gems;
 import com.igauntlet.common.damage.IDamageSource;
 import com.igauntlet.common.entity.EntityLaser;
 import com.igauntlet.common.tileentity.TileAshPile;
-import com.igauntlet.config.ModConfig;
-import com.igauntlet.init.ModBlocks;
 import com.igauntlet.init.ModItems;
 import com.igauntlet.util.handlers.SoundsHandler;
-import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 
 public class GemPower {
-
-    // Not in use , Current is a Packet (MessageSnap)
-    public static void Snap(EntityPlayer playerIn, ItemStack stack, int extend) {
-        NBTTagCompound nbt = stack.getTagCompound();
-
-        boolean CanSnap = ModConfig.Gauntlet.Snap;
-        if (playerIn.isSneaking() && CanSnap) {
-            playerIn.world.playSound(null, playerIn.getPosition(), SoundsHandler.SNAP, SoundCategory.HOSTILE, 1F, 1F);
-        }
-
-        if (playerIn.isSneaking() && CanSnap) {
-            for (Entity targetentity : playerIn.world.getEntitiesWithinAABB(EntityLiving.class, playerIn.getEntityBoundingBox().grow(extend, extend, extend))) {
-                int entity = targetentity.getEntityId();
-
-                Block blk = ModBlocks.ASH_PILE;
-                BlockPos pos0 = new BlockPos(targetentity.posX, targetentity.posY, targetentity.posZ);
-                IBlockState state0 = blk.getDefaultState();
-                targetentity.world.setBlockState(pos0, state0);
-                WriteAsh(pos0, playerIn.world, entity);
-                targetentity.attackEntityFrom(IDamageSource.SNAP, 1000);
-
-                if (!playerIn.capabilities.isCreativeMode) {
-                    stack.setItemDamage(stack.getItemDamage() + 1);
-                }
-            }
-            nbt.setBoolean("snapnow", false);
-            stack.writeToNBT(nbt);
-        }
-    }
 
     public static void Laser(EntityPlayer entityplayer, World worldIn, ItemStack stack) {
         if (!entityplayer.isSneaking() && entityplayer.getHeldItemOffhand().getItem() != ModItems.INFINITY_GAUNTLET) {
