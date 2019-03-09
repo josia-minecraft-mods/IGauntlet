c74492933884f90baa1ba3478321b2f605a12753
Josia50 <elwyn.vdb@gmail.com>
2019-03-09 17:35:31 +0100

    Update 3.3

--- src/main/java/com/igauntlet/Infinity.java
+++ src/main/java/com/igauntlet/Infinity.java
@@ -19,9 +19,8 @@ public class Infinity {
     @Mod.Instance
     public static Infinity instance;
 
-
     public static final String NAME = "IGauntlet Mod";
-    public static final String VERSION = "3.2";
+    public static final String VERSION = "3.3";
     public static final String MODID = "igauntlet";
     public static final String UPDATEURL = "https://raw.githubusercontent.com/rootheworld/NewInfinityGauntlet/master/update.json";
 
