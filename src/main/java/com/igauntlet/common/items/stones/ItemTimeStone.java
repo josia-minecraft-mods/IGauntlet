4039c44a035d053f87266d2f630a892514df5ae2
Josia50 <elwyn.vdb@gmail.com>
2019-04-03 08:57:22 +0200

    Damage on Hold

--- src/main/java/com/igauntlet/common/items/stones/ItemTimeStone.java
+++ src/main/java/com/igauntlet/common/items/stones/ItemTimeStone.java
@@ -1,9 +1,11 @@
 package com.igauntlet.common.items.stones;
 
 import com.igauntlet.Infinity;
+import com.igauntlet.common.damage.IDamageSource;
 import com.igauntlet.init.InfinityConfig;
 import com.igauntlet.init.InfinityItems;
 import com.igauntlet.tabs.InfinityTabs;
+import com.igauntlet.util.helpers.EntityHelper;
 import com.igauntlet.util.helpers.IHasModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
@@ -11,6 +13,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemTimeStone extends Item implements IHasModel {
+    private int time = 0;
 
     public ItemTimeStone(String name) {
         setTranslationKey(name);
@@ -27,13 +30,18 @@ public class ItemTimeStone extends Item implements IHasModel {
         Infinity.proxy.registerItemRenderer(this, 0, "inventory");
     }
 
-
     @Override
     public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
 
-        if(isSelected) {
+        if (isSelected && InfinityConfig.AllowedGems.TimeStone) {
             worldIn.getWorldInfo().setWorldTime(worldIn.getWorldTime() + InfinityConfig.Gauntlet.TimeStone.TimeSpeed);
+            time++;
+            if (time >= 200)
+                EntityHelper.AttackBySource(entityIn, IDamageSource.HOLDING, 0.001F);
+            if (time == 201)
+                time = 0;
+            System.out.println(time);
         }
     }
 }
