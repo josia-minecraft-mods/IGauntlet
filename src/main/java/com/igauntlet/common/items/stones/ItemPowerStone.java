c74d41ac509fc057d8746ea841902dec0d2f57b6
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-08 16:16:00 -0500

    idk

--- src/main/java/com/igauntlet/common/items/stones/ItemPowerStone.java
+++ src/main/java/com/igauntlet/common/items/stones/ItemPowerStone.java
@@ -34,7 +34,7 @@ public class ItemPowerStone extends Item implements IHasModel {
     int min = 1;
     int max = 7;
     int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
-    if randomNum == 3 {
+    if (randomNum == 3) {
         PlayerHelper.sendMessageClient(player, "stones.power.spaired", true);
     }
     else {
