c9d4b7ab1e55946370b112fc3ad7a59fd4562b59
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-08 08:38:44 -0500

    powerstone

--- src/main/java/com/igauntlet/common/items/stones/ItemPowerStone.java
+++ src/main/java/com/igauntlet/common/items/stones/ItemPowerStone.java
@@ -4,7 +4,13 @@ import com.igauntlet.Infinity;
 import com.igauntlet.init.InfinityItems;
 import com.igauntlet.tabs.InfinityTabs;
 import com.igauntlet.util.helpers.IHasModel;
+import com.igauntlet.util.helpers.PlayerHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ItemPowerStone extends Item implements IHasModel {
@@ -21,19 +27,20 @@ public class ItemPowerStone extends Item implements IHasModel {
 
 @Override
     public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-        super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+    super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
 
 
-        EntityPlayer player = (EntityPlayer) entityIn;
-         int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1)
-             var min == 1
-             var max == 7
-             if randomNum == 1-6 {
-                 target.setDead();
-                 }
-        else{
-            PlayerHelper.sendMessageClient(player, "stones.power.spaired", true);
-            }
+    EntityPlayer player = (EntityPlayer) entityIn;
+    int min = 1;
+    int max = 7;
+    int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+    if randomNum == 1, 2, 3, 4, 5, 6 {
+        player.setDead();
+    }
+    else {
+        PlayerHelper.sendMessageClient(player, "stones.power.spaired", true);
+    }
+}
              
      @Override
     public void registerModels() {
