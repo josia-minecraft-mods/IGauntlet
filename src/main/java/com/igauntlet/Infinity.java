0811265d94a3df8f88c96d7ab63b216ae4cbca93
Josia50 <elwyn.vdb@gmail.com>
2019-04-18 14:57:14 +0200

    Preparing 3.5 Update

--- src/main/java/com/igauntlet/Infinity.java
+++ src/main/java/com/igauntlet/Infinity.java
@@ -16,13 +16,13 @@ import org.apache.logging.log4j.Logger;
 public class Infinity {
 
     //TODO Rewrite the whole mod when the mod is fully released. Easier for debugging and stuff later
-    // TODO New Registries and stuff , Moving away from the tuturials aka learning process.
+    //TODO New Registries and stuff , Moving away from the tutorials aka learning process.
 
     @Mod.Instance
     public static Infinity instance;
 
     public static final String NAME = "IGauntlet Mod";
-    public static final String VERSION = "3.4";
+    public static final String VERSION = "3.5";
     public static final String MODID = "igauntlet";
     public static final String UPDATEURL = "https://raw.githubusercontent.com/josia-minecraft-mods/IGauntlet/master/update.json";
 
