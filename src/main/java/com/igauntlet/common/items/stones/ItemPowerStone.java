cbe431949738a8a2f331bbe098133f3cccdc0623
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-19 15:25:12 -0500

    powerstone random worthyness check

--- src/main/java/com/igauntlet/common/items/stones/ItemPowerStone.java
+++ src/main/java/com/igauntlet/common/items/stones/ItemPowerStone.java
@@ -1,7 +1,15 @@
 package com.igauntlet.common.items.stones;
 
 import com.igauntlet.tabs.InfinityTabs;
+import com.igauntlet.util.helpers.PlayerHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ItemPowerStone extends Item {
 
@@ -11,5 +19,21 @@ public class ItemPowerStone extends Item {
         setCreativeTab(InfinityTabs.infinityTabs);
         setMaxStackSize(1);
         setMaxDamage(4500);
+    }
+
+    @Override
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+
+
+        EntityPlayer player = (EntityPlayer) entityIn;
+        Random random = new Random();
+        int randomn = random.nextInt(8);
+        if (randomn == 3) {
+            PlayerHelper.sendMessageClient(player, "stones.power.spaired", true);
+        }
+        else {
+            player.setDead();
+        }
     }
 }
