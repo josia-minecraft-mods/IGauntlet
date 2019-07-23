8ea3d4bba655d3f1249336d32034e035d3e0788d
CoolDude145 <alexmovie@net.hr>
2019-07-23 13:48:14 +0200

    The textures are really bugging me, hope you can fix it :)

---
+++ src/main/java/com/jmteam/igauntlet/common/items/tools/ItemMjolnir.java
@@ -0,0 +1,16 @@
+package com.jmteam.igauntlet.common.items.tools;
+
+import com.jmteam.igauntlet.tabs.InfinityTabs;
+import net.minecraft.item.Item;
+
+
+public class ItemMjolnir extends Item {
+
+    public ItemMjolnir(String name) {
+        setTranslationKey(name);
+        setRegistryName(name);
+        setCreativeTab(InfinityTabs.infinityTabs);
+        setMaxStackSize(1);
+        setMaxDamage(5000);
+    }
+}
