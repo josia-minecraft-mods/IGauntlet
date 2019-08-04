3ecb1c2c62bd2357a29d7c9213d1081a6682ca29
Josia50 <elwyn.vdb@gmail.com>
2019-08-04 22:56:15 +0200

    Smaller Fixes

--- src/main/java/com/jmteam/igauntlet/util/handlers/client/RenderHandler.java
+++ src/main/java/com/jmteam/igauntlet/util/handlers/client/RenderHandler.java
@@ -6,10 +6,10 @@ import com.jmteam.igauntlet.client.render.RenderSquirrelGirl;
 import com.jmteam.igauntlet.common.entity.EntityLaser;
 import com.jmteam.igauntlet.common.entity.EntityPortal;
 import com.jmteam.igauntlet.common.entity.EntitySquirrelGirl;
-import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class RenderHandler {
+
     public static void registerEntityRenders() {
         RenderingRegistry.registerEntityRenderingHandler(EntityLaser.class, RenderLaser::new);
         RenderingRegistry.registerEntityRenderingHandler(EntityPortal.class, RenderPortal::new);
