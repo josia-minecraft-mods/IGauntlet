ef4c9b77b2e3b51fec1591a95a6db30d4a13e2ad
Josia50 <elwyn.vdb@gmail.com>
2019-08-04 23:05:33 +0200

    GUI and Interaction

--- src/main/java/com/jmteam/igauntlet/common/items/ItemWalkman.java
+++ src/main/java/com/jmteam/igauntlet/common/items/ItemWalkman.java
@@ -1,8 +1,19 @@
 package com.jmteam.igauntlet.common.items;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 public class ItemWalkman extends Item {
 
     public ItemWalkman() {}
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+     // TODO open GUI with container
+        return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
 }
