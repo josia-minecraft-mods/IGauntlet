7c133aae508c332142cf1da12055dbc84798828f
Josia <elwyn.vdb@gmail.com>
2020-05-09 16:57:20 +0200

    Helper

--- src/main/java/com/jmteam/igauntlet/util/gauntlet/GauntletHelper.java
+++ src/main/java/com/jmteam/igauntlet/util/gauntlet/GauntletHelper.java
@@ -1,7 +1,22 @@
 package com.jmteam.igauntlet.util.gauntlet;
 
+import com.jmteam.igauntlet.common.init.InfinityNBT;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+
 public class GauntletHelper {
 
+    public static GemHelper.StoneType getActiveStone(ItemStack stack) {
+        CompoundNBT nbt = stack.getTag();
 
+        if(nbt != null && nbt.contains(InfinityNBT.SELECTED_STONE)) {
+            return GemHelper.StoneType.valueOf(nbt.getString(InfinityNBT.SELECTED_STONE));
+        }else{
+            if(nbt == null) {
+                stack.setTag(new CompoundNBT());
+            }
+        }
 
+        return GemHelper.StoneType.NONE;
+    }
 }
