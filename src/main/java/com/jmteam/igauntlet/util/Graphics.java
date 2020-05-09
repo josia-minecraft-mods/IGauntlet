9a55d86c1d53ccdcf062c111b516f419818720ed
Josia <elwyn.vdb@gmail.com>
2020-05-09 09:50:52 +0200

    Graphics

---
+++ src/main/java/com/jmteam/igauntlet/util/Graphics.java
@@ -0,0 +1,22 @@
+package com.jmteam.igauntlet.util;
+
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+
+public class Graphics {
+
+    private static float zLevel;
+    public static float RENDER_NUM = 0.0625f;
+
+    public static void drawTexturedModalRect(int x, int y, int textureX, int textureY, int width, int height)
+    {
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos((x + 0), (y + height), zLevel).tex(((float)(textureX + 0) * 0.00390625F), ((float)(textureY + height) * 0.00390625F)).endVertex();
+        bufferbuilder.pos((x + width), (y + height), zLevel).tex(((float)(textureX + width) * 0.00390625F), ((float)(textureY + height) * 0.00390625F)).endVertex();
+        bufferbuilder.pos((x + width), (y + 0), zLevel).tex(((float)(textureX + width) * 0.00390625F), ((float)(textureY + 0) * 0.00390625F)).endVertex();
+        bufferbuilder.pos((x + 0), (y + 0),zLevel).tex(((float)(textureX + 0) * 0.00390625F), ((float)(textureY + 0) * 0.00390625F)).endVertex();
+        tessellator.draw();
+    }}
