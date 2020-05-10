e5ade306149b710f6ae6589dedb7416bae8ce198
ukminecrafted <ukminecrafted@gmail.com>
2020-05-09 19:38:44 -0500

    fixed typo

--- src/main/java/com/jmteam/igauntlet/common/init/InfinityTileentities.java
+++ src/main/java/com/jmteam/igauntlet/common/init/InfinityTileentities.java
@@ -1,7 +1,7 @@
 package com.jmteam.igauntlet.common.init;
 
 import com.jmteam.igauntlet.Infinity;
-import com.jmteam.igauntlet.common.tileentity.TileEnityTesseract;
+import com.jmteam.igauntlet.common.tileentity.TileEntityTesseract;
 import com.jmteam.igauntlet.common.tileentity.TileEntityAshPile;
 import com.jmteam.igauntlet.common.tileentity.TileEntityQuickSand;
 import net.minecraft.tileentity.TileEntity;
@@ -11,7 +11,7 @@ public class InfinityTileentities {
 
     public static final void PreInit() {
         registerTileEntity(TileEntityAshPile.class, "ash_pile");
-        registerTileEntity(TileEnityTesseract.class, "tesseract");
+        registerTileEntity(TileEntityTesseract.class, "tesseract");
         registerTileEntity(TileEntityQuickSand.class, "quicksand");
     }
 
