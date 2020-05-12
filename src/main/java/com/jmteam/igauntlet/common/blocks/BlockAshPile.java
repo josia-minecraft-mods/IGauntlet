6d494977950721623c9e53290f96ce1c01cda91f
Josia <elwyn.vdb@gmail.com>
2020-05-12 23:35:14 +0200

    Ash Piles, Needs testing, i'll try with snap packet soon

--- src/main/java/com/jmteam/igauntlet/common/blocks/BlockAshPile.java
+++ src/main/java/com/jmteam/igauntlet/common/blocks/BlockAshPile.java
@@ -8,14 +8,19 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
--- src/main/java/com/jmteam/igauntlet/util/gauntlet/GemHelper.java
+++ src/main/java/com/jmteam/igauntlet/util/gauntlet/GemHelper.java
@@ -1,10 +1,10 @@
 package com.jmteam.igauntlet.util.gauntlet;
 
-import com.jmteam.igauntlet.common.blocks.InfinityBlock;
 import com.jmteam.igauntlet.common.init.InfinityBlocks;
 import com.jmteam.igauntlet.common.tileentity.TileEntityAshPile;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
@@ -12,28 +12,50 @@ import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 
 import java.util.Iterator;
-import java.util.stream.Collectors;
 
 public class GemHelper {
 
     public static void reviveAshPiles(PlayerEntity player, int range) {
         World world = player.world;
-        Iterator<BlockPos> blockPosIterator =  BlockPos.getAllInBox(player.getPosition().subtract(new Vec3i(range, range, range)), player.getPosition().add(new Vec3i(range, range, range))).iterator();
+        Iterator<BlockPos> blockPosIterator = BlockPos.getAllInBox(player.getPosition().subtract(new Vec3i(range, range, range)), player.getPosition().add(new Vec3i(range, range, range))).iterator();
 
-        if(blockPosIterator.hasNext()) {
+        if (blockPosIterator.hasNext()) {
             BlockPos pos = blockPosIterator.next();
             BlockState state = world.getBlockState(pos);
 
-            if(state.getBlock() == InfinityBlocks.ash_pile) {
+            if (state.getBlock() == InfinityBlocks.ash_pile) {
                 TileEntity te = world.getTileEntity(pos);
 
-                if(te != null && te instanceof TileEntityAshPile) {
+                if (te != null && te instanceof TileEntityAshPile) {
                     TileEntityAshPile ashPile = (TileEntityAshPile) te;
                     world.addEntity(ashPile.getEntity());
                     world.setBlockState(pos, Blocks.AIR.getDefaultState());
                 }
             }
         }
+    }
+
+    public static boolean createAshPile(World world, BlockPos pos, LivingEntity entity) {
+        if (entity != null) {
+            BlockPos placePos = pos;
+
+            if (world.getBlockState(placePos).getBlock() != Blocks.AIR) {
+                if (world.getBlockState(pos.up()).getBlock() != Blocks.AIR) return false;
+
+                placePos = pos.up();
+            }
+
+            world.setBlockState(placePos, InfinityBlocks.ash_pile.getDefaultState());
+            TileEntity te = world.getTileEntity(placePos);
+
+            if (te != null && te instanceof TileEntityAshPile) {
+                TileEntityAshPile ashPile = (TileEntityAshPile) te;
+                ashPile.setEntity(entity);
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public enum StoneType {
