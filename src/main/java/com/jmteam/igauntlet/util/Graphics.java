60752d8f95a631884fc28b3864eac33fa1b5d1a6
Josia <38124619+Josia50@users.noreply.github.com>
2020-06-20 20:25:41 +0200

    Update Graphics.java

--- src/main/java/com/jmteam/igauntlet/util/Graphics.java
+++ src/main/java/com/jmteam/igauntlet/util/Graphics.java
@@ -13,10 +13,10 @@ public class Graphics {
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferbuilder = tessellator.getBuffer();
         bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-        bufferbuilder.pos((x + 0), (y + height), zLevel).tex(((float) (textureX + 0) * 0.00390625F), ((float) (textureY + height) * 0.00390625F)).endVertex();
+        bufferbuilder.pos(x, (y + height), zLevel).tex(((float) textureX * 0.00390625F), ((float) (textureY + height) * 0.00390625F)).endVertex();
         bufferbuilder.pos((x + width), (y + height), zLevel).tex(((float) (textureX + width) * 0.00390625F), ((float) (textureY + height) * 0.00390625F)).endVertex();
-        bufferbuilder.pos((x + width), (y + 0), zLevel).tex(((float) (textureX + width) * 0.00390625F), ((float) (textureY + 0) * 0.00390625F)).endVertex();
-        bufferbuilder.pos((x + 0), (y + 0), zLevel).tex(((float) (textureX + 0) * 0.00390625F), ((float) (textureY + 0) * 0.00390625F)).endVertex();
+        bufferbuilder.pos((x + width), y, zLevel).tex(((float) (textureX + width) * 0.00390625F), ((float) textureY * 0.00390625F)).endVertex();
+        bufferbuilder.pos(x, y, zLevel).tex(((float) (textureX + 0) * 0.00390625F), ((float) textureY * 0.00390625F)).endVertex();
         tessellator.draw();
     }
 }
