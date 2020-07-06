8469a60d6dfdd7cc6323351b15d3714ba951473e
Josia <elwyn.vdb@gmail.com>
2020-07-06 21:28:32 +0200

    Time's moving faster

--- src/main/java/com/jmteam/igauntlet/common/item/stones/ItemTimeStone.java
+++ src/main/java/com/jmteam/igauntlet/common/item/stones/ItemTimeStone.java
@@ -1,10 +1,20 @@
 package com.jmteam.igauntlet.common.item.stones;
 
 import com.jmteam.igauntlet.common.item.ItemBase;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemTimeStone extends ItemStoneBase {
 
     public ItemTimeStone() {
         super();
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(isSelected) {
+            worldIn.setDayTime(worldIn.getDayTime() + 10);
+        }
     }
 }
